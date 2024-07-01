23f69d2d134545f0a71e6236b533dd79ac50c4ce
Inshiya Shabbirbhai Ravat <insravat@gmail.com>
2024-07-01 14:23:11 +0530

    resolved import errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import axios from'axios'
 
 function App() {
   const [selectedFile, setSelectedFile] = useState(null)
