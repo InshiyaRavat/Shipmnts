38d850d0ddb7568b68727e9ee93d5fc92855b599
Inshiya Shabbirbhai Ravat <insravat@gmail.com>
2024-07-01 13:10:56 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+app.use(express.json())
+app.use(cors({
+    origin : ["http://localhost:3000"],
+    methods : ['GET','POST','PUT','DELETE']
+}))
+
+const port = process.env.port
+mongoose.connect(process.env.MONGODB_URI).then(console.log("db connected")).catch("db not connected")
+
+app.listen(port,()=>{
+    console.log(`server is listening on port ${port}`)
+})
