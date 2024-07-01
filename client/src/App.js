512615e12cf814d851c42a630030d4141ed57083
Inshiya Shabbirbhai Ravat <insravat@gmail.com>
2024-07-01 14:21:04 +0530

    created form

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,40 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState(null)
+  function handleChange(e){
+    e.preventDefault();
+    setSelectedFile(e.target.files[0])
+    if(selectedFile.size > 1024){
+      alert('file size exceeded 1MB')
+    }
+  }
+  async function handleSubmit(e){
+    e.preventDefault()
+    const formData = new FormData()
+    formData.append('excel',selectedFile)
+    try{
+      const response = await axios.post("http://localhost:5000/upload",formData)
+      if(response.ok){
+        alert("file uploaded successfully!!")
+      }
+      else{
+        alert("erroe occured in uploading file")
+      }
+    }
+    catch(err){
+      console.log(err)
+      alert("erroe occured in uploading file")
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit}>
+          <label>upload file(.xls / .xlsx)</label>
+          <input type='file' onChange={handleChange}/>
+          <button type='submit'>submit</button>
+        </form>
       </header>
     </div>
   );
